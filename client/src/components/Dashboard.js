f58349d870497b7de1615bab6e727c49b132d24c
MauricioRosasKsquare <Mauricio.Rosas@theksquaregroup.com>
2021-11-30 00:55:30 -0600

    Chats

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,4 +1,4 @@
-//import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 function Dashboard( { tickets } ) {
     
@@ -7,8 +7,12 @@ function Dashboard( { tickets } ) {
     var totalSell = 0
     var totalEarned = 0
     var sellersStats = []
+    var sellersStatsPro = []
+    var sellersStatsSuperPro = []
     var bestSellers = []
     var worstSellers = []
+
+    
     
     tickets.map(ticket =>{
         
@@ -24,25 +28,79 @@ function Dashboard( { tickets } ) {
         return [totalCancel, totalCancelLost,totalSell,totalEarned]
     })
 
+    tickets.map(ticket =>{
+        ticket.products.forEach(product =>{
+            sellersStats.push({name:product.name, qty:product.qty })
+        })
+        return true         
+    })
 
-    // tickets.map(ticket =>{
-    //     ticket.products.map(product =>{
-            
-    //         sellersStats.find( {name: product.name}).then(res => {})
-                
-            
-    //     })
-    // })
+    sellersStats.forEach(product =>{
+        let exist = sellersStatsPro.findIndex( (obj) => obj.name == product.name)
+        if(exist === -1){
+            sellersStatsPro.push(product)
+        }else{
+            sellersStatsPro[exist] = {name: product.name,
+                                    qty: sellersStatsPro[exist].qty + product.qty
+                                    }
+        }
+    })
+
+
+    sellersStatsSuperPro = sellersStatsPro.sort((a, b) => {
+        return a.qty - b.qty;
+    });
     
-    
-    
+    bestSellers = sellersStatsSuperPro.slice(-5)
+    worstSellers = sellersStatsSuperPro.slice(0,5)
+
     return (
         <>
-        <div>
+        <div className="wrapper2">
             <h1>Total Cancelations: {totalCancel}</h1>
             <h1>Total money lost from Cancelations: ${totalCancelLost}</h1>
             <h1>Total Sells: {totalSell}</h1>
             <h1>Total money earned: ${totalEarned}</h1>
+        </div>
+        <div className="wrapper2">
+            <h2 style={{textAlign:"center"}}>Best Sellers</h2>
+            <BarChart
+            width={700}
+            height={300}
+            data={bestSellers}
+            margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+            }}
+            >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="qty" fill="#8884d8" />
+            </BarChart> 
+        </div>
+        <div className="wrapper2">
+            <h2 style={{textAlign:"center"}}>Worst Sellers</h2>
+            <BarChart
+            width={700}
+            height={300}
+            data={worstSellers}
+            margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+            }}
+            >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Bar dataKey="qty" fill="#8884d8" />
+            </BarChart> 
         </div>
         </>
     )
